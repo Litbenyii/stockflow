de32457350b6eb2762cac71c7d10e7a650602ebd
Litbenyii <diazb9737@gmail.com>
2026-03-23 15:37:17 -0300

    Configure Astro site settings and output type

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://litbenyii.github.io',
+  base: '/stockflow',
+  output: 'static'
+});
